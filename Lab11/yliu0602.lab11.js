3aba3113b3f9197677aecc8435dafc33d8c7d295
akumaluffy <yliu0602@gmail.com>
2023-11-26 14:48:54 -0600

    why is api key not working???

--- Lab11/yliu0602.lab11.js
+++ Lab11/yliu0602.lab11.js
@@ -1,9 +1,22 @@
+const API_KEY = "AIzaSyBSn988LMb-dtaYkGeGEpG3PvVzyYcgWFY";
+
 const search_button = document.querySelector(".search-button");
 
 search_button.addEventListener("click", () => {
-    let user = document.getElementById("search-box").value;
+    const user = document.getElementById("search-box").value;
+    // const user_author = document.getElementById("author-box").value.split(" ").join("");
     console.log(user);
 
     const book_list = document.querySelector(".book-list");
-    
+
+    // fetch api data
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(user)}&key=${API_KEY}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.error("There has been a problem with your fetch operation:", error));
 })
