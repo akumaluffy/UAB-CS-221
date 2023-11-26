65d0c8995bc3a22cef90ee8bc7c1f0d38eabe2da
akumaluffy <yliu0602@gmail.com>
2023-11-26 13:33:00 -0600

    lab 11

--- Lab11/yliu0602.lab11.js
+++ Lab11/yliu0602.lab11.js
@@ -1,1 +1,9 @@
-let 
+const search_button = document.querySelector(".search-button");
+
+search_button.addEventListener("click", () => {
+    let user = document.getElementById("search-box").value;
+    console.log(user);
+
+    const book_list = document.querySelector(".book-list");
+    
+})
